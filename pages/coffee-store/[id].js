9212d442caa6d689a7be12be49dfab7be97434c3
Depth-Hoar <44712760+Depth-Hoar@users.noreply.github.com>
2023-08-22 07:32:37 -0600

    getStaticPaths

--- pages/coffee-store/[id].js
+++ pages/coffee-store/[id].js
@@ -1,15 +1,52 @@
 import { useRouter } from "next/router";
 import Link from "next/link";
+import Head from "next/head";
 
-const CoffeeStore = () => {
+import coffeeStoresData from "../../data/coffee-stores.json";
+
+export async function getStaticProps(staticProps) {
+  const params = staticProps.params;
+  console.log("params", params);
+  return {
+    props: {
+      coffeeStore: coffeeStoresData.find((coffeeStore) => {
+        return coffeeStore.id.toString() === params.id; //dynamic id
+      }),
+    },
+  };
+}
+
+export function getStaticPaths() {
+  const paths = coffeeStoresData.map((coffeeStore) => {
+    return {
+      params: {
+        id: coffeeStore.id.toString(),
+      },
+    };
+  });
+  return {
+    paths,
+    fallback: true,
+  };
+}
+
+const CoffeeStore = (props) => {
   const router = useRouter();
   console.log("router", router);
+  
+  if (router.isFallback) {
+    return <div>Loading...</div>;
+  }
+  const { address, name, neighbourhood } = props.coffeeStore;
+  console.log("props", props);
   return (
     <div>
-      Coffee Store Page {router.query.id}
-      <Link href="/">
-        Back to home
-      </Link>
+      <Head>
+        <title>{name}</title>
+      </Head>
+      <p>{address}</p>
+      <p>{name}</p>
+      <p>{neighbourhood}</p>
     </div>
   );
 };
--- pages/index.js
+++ pages/index.js
@@ -5,12 +5,12 @@ import styles from "../styles/Home.module.css";
 import Banner from "../components/banner";
 import Card from "../components/card";
 
-import coffeeStores from "../data/coffee-stores.json";
+import coffeeStoresData from "../data/coffee-stores.json";
 
 export async function getStaticProps(context) {
   return {
     props: {
-      coffeeStores,
+      coffeeStores: coffeeStoresData,
     }, // will be passed to the page component as props
   };
 }
@@ -37,19 +37,24 @@ export default function Home(props) {
           <div className={styles.heroImage}>
           <Image src="/static/hero-image.png" width={700} height={400} alt="hero image" />
         </div>
-        <div className={styles.cardLayout}>
-        {props.coffeeStores.map((coffeeStore) => {
-            return (
-              <Card
-                key={coffeeStore.id}
-                name={coffeeStore.name}
-                imgUrl={coffeeStore.imgUrl}
-                href={`/coffee-store/${coffeeStore.id}`}
-                className={styles.card}
-              />
-            );
-          })}
-        </div>
+        {props.coffeeStores.length > 0 && (
+          <>
+            <h2 className={styles.heading2}>Calgary Stores</h2>
+            <div className={styles.cardLayout}>
+              {props.coffeeStores.map((coffeeStore) => {
+                return (
+                  <Card
+                    key={coffeeStore.id}
+                    name={coffeeStore.name}
+                    imgUrl={coffeeStore.imgUrl}
+                    href={`/coffee-store/${coffeeStore.id}`}
+                    className={styles.card}
+                  />
+                );
+              })}
+            </div>
+          </>
+        )}
       </main>
     </div>
   );
