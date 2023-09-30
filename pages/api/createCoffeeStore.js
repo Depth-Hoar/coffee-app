a6cd92d9862fd33944d7f5541c14ba3422bcaee6
Depth-Hoar <44712760+Depth-Hoar@users.noreply.github.com>
2023-09-30 10:31:00 -0600

    create coffee store on airtable

--- pages/api/createCoffeeStore.js
+++ pages/api/createCoffeeStore.js
@@ -5,15 +5,59 @@ const base = new Airtable({ apiKey: process.env.AIR_TABLE_TOKEN }).base(
 
 const table = base("coffee-stores");
 
-console.log({ table });
+console.log({ table }, "TABLESSS");
 
-const createCoffeeStore = (req, res) => {
+const createCoffeeStore = async (req, res) => {
   console.log({ req });
+
   if (req.method === "POST") {
-    res.json({ message: "Hi there" });
-  } else {
-    res.json({ message: "method is GET" });
+       //find a record
+       const { id, name, neighbourhood, address, imgUrl, voting } = req.body;
+
+      try {
+      const findCoffeeStoreRecords = await table
+        .select({
+          filterByFormula: `id=${id}`,
+        })
+        .firstPage();
+ 
+      console.log({ findCoffeeStoreRecords });
+ 
+      if (findCoffeeStoreRecords.length !== 0) {
+        const records = findCoffeeStoreRecords.map((record) => {
+          return {
+            ...record.fields,
+          };
+        });
+        res.json(records);
+      } else {
+        //create a record
+        const createRecords = await table.create([
+          {
+            fields: {
+              id,
+              name,
+              address,
+              neighbourhood,
+              voting,
+              imgUrl,
+            },
+          },
+        ]);
+        // res.json({ message: "create a record", records: createRecords });
+        const records = createRecords.map((record) => {
+          return {
+            ...record.fields,
+          };
+        });
+        res.json(records);
+      }
+    } catch (err) {
+      console.error("Error finding store", err);
+      res.status(500);
+      res.json({ message: "Error finding store", err });
+     }
   }
 };
 
-export default createCoffeeStore;
+export default createCoffeeStore
