1f24f9a5a068c1a4347778bf0d1b854c713d3c4d
Depth-Hoar <44712760+Depth-Hoar@users.noreply.github.com>
2023-10-02 07:08:30 -0600

    actually refactoring

--- pages/api/createCoffeeStore.js
+++ pages/api/createCoffeeStore.js
@@ -1,35 +1,19 @@
-const Airtable = require("airtable");
-const base = new Airtable({ apiKey: process.env.AIR_TABLE_TOKEN }).base(
-  process.env.AIR_TABLE_BASE_KEY
-);
-
-const table = base("coffee-stores");
-
-console.log({ table }, "TABLESSS");
+import { table, getMinifiedRecords } from "../../lib/airtable";
 
 const createCoffeeStore = async (req, res) => {
-  console.log({ req });
-
   if (req.method === "POST") {
        //find a record
        const { id, name, neighbourhood, address, imgUrl, voting } = req.body;
-
       try {
         if (id) {
           const findCoffeeStoreRecords = await table
             .select({
-              filterByFormula: `id=${id}`,
+              filterByFormula: `id="${id}"`,
             })
             .firstPage();
  
-      console.log({ findCoffeeStoreRecords });
- 
       if (findCoffeeStoreRecords.length !== 0) {
-        const records = findCoffeeStoreRecords.map((record) => {
-          return {
-            ...record.fields,
-          };
-        });
+        const records = getMinifiedRecords(findCoffeeStoreRecords);
         res.json(records);
       } else {
         //create a record
@@ -47,11 +31,7 @@ const createCoffeeStore = async (req, res) => {
             },
           ]);
         // res.json({ message: "create a record", records: createRecords });
-        const records = createRecords.map((record) => {
-          return {
-            ...record.fields,
-          };
-        });
+        const records = getMinifiedRecords(createRecords);
         res.json(records);
       } else {
         res.status(400);
